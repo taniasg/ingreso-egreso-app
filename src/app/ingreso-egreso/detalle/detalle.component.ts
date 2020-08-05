1c04ac2251d3a4ccf8e90d6354277dfa796892ad
Tania Sanchez <tania.sanchez16@live.com>
2020-08-05 17:48:15 -0500

    Correccion de importacion

--- src/app/ingreso-egreso/detalle/detalle.component.ts
+++ src/app/ingreso-egreso/detalle/detalle.component.ts
@@ -4,6 +4,7 @@ import { IngresoEgreso } from '../../models/ingreso-egreso.model';
 import { Subscription } from 'rxjs';
 import { IngresoEgresoService } from '../../services/ingreso-egreso.service';
 import Swal from 'sweetalert2';
+import { AppStateWithIngreso } from '../ingreso-egreso.reducer';
 
 @Component({
   selector: 'app-detalle',
